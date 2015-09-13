78e2107ba094ca4104238a542c1afd0ef4ffdce5
Jeff Gerber <jallengerber@gmail.com>
2015-09-13 14:51:25 -0700

    basic functionality achieved

--- test/framework_test.js
+++ test/framework_test.js
@@ -4,20 +4,22 @@ var chai = require('chai');
 var chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 var expect = chai.expect;
+var http = require('http');
+var Frame = require(__dirname + '/../lib/router');
 
-var frame = require(__dirname + '/../lib/framework');
+var frame = new Frame();
 
 var host = 'localhost:3000';
 
 describe('framework', function() {
   before(function() {
+    http.createServer(frame.route).listen(3000, function() {
+      console.log('server started');
+    });
+
     frame.get('/', function(req, res) {
       res.set('Content-Type', 'text/plain');
       res.send("Hello, world"); //this write the header, set the body
-    });
-
-    frame.listen(3000, function() {
-      console.log('server started');
     });
   });
 
@@ -26,7 +28,7 @@ describe('framework', function() {
       .get('/')
       .end(function(err, res) {
         expect(res.status).to.eql(200);
-        expect(res.header['content-type']).to.eql('text/plain; charset=utf-8');
+        expect(res.header['content-type']).to.eql('text/plain');
         expect(res.text).to.eql('Hello, world');
         done();
       });
